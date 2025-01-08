91e67203f03d90ef8b0d3c71a144b33f2b519e7c
Chris Boot <chris@bootcom.co.uk>
2025-01-08 15:57:25 +0000

    New PayloadModel model created required for syncing

---
+++ Common/MongoDB.Sync.Web/Models/SyncModels/PayloadModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace MongoDB.Sync.Web.Models.SyncModels
+{
+    public class PayloadModel
+    {
+
+        [BsonElement("action")]
+        public string Action { get; set; }
+
+        [BsonElement("collection")]
+        public string Collection { get; set; }
+
+        [BsonElement("document")]
+        public Object Document { get; set; }
+
+        [BsonElement("appId")]
+        public string AppId { get; set; }
+
+    }
+}
