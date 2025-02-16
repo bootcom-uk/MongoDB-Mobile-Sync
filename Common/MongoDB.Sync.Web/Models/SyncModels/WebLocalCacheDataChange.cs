6e15bceb2811366dda8e44872f03fe8d3ce2bf95
Chris Boot <chris@bootcom.co.uk>
2025-02-16 10:07:30 +0000

    Directory cleanup Add references I originally forgot about! Functionality to write our change to MongoDB

---
+++ Common/MongoDB.Sync.Web/Models/SyncModels/WebLocalCacheDataChange.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Sync.Models;
+using MongoDB.Sync.Models.Attributes;
+
+namespace MongoDB.Sync.Web.Models.SyncModels
+{
+    public class WebLocalCacheDataChange : LocalCacheDataChange
+    {
+
+        /// <summary>
+        /// Specifies the unique identifier of the change
+        /// </summary>
+        [DatabaseFieldName("_id")]
+        public ObjectId Id { get; set; }
+
+
+        /// <summary>
+        /// Where the action is not a deletion, this contains the document of changes
+        /// </summary>
+        public BsonDocument? Document { get; set; }
+
+    }
+}
