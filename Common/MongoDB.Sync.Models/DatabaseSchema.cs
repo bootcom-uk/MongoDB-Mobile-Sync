065ecba399fd41c5cca12b466d22df0df4a23489
Chris Boot <chris@bootcom.co.uk>
2025-03-15 20:17:39 +0000

    Include strongly typed models

---
+++ Common/MongoDB.Sync.Models/DatabaseSchema.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoDB.Sync.Models
+{
+    public class DatabaseSchema
+    {
+        public string DatabaseName { get; set; }
+        public List<CollectionSchema> Collections { get; set; }
+    }
+}
---
+++ Common/MongoDB.Sync.Models/FieldSchema.cs
@@ -0,0 +1,9 @@
+﻿
+namespace MongoDB.Sync.Models
+{
+    public class FieldSchema
+    {
+        public string Name { get; set; }
+        public FieldType Type { get; set; }
+    }
+}
