5669f24001529e10fb1b62d4ffaef1b560faeb42
Chris Boot <chris@bootcom.co.uk>
2025-07-06 20:58:55 +0100

    Implement extensions class

---
+++ Common/MongoDB.Sync/Extensions/LiveQueryableLiteCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Sync.Core.Services.Models.Models;
+using MongoDB.Sync.LocalDataCache;
+
+namespace MongoDB.Sync.Extensions
+{
+    public static class LiveQueryableLiteCollectionExtensions
+    {
+
+        public static void AddRange<T>(this LiveQueryableLiteCollection<T> collection, IEnumerable<T> items) where T : BaseLocalCacheModel
+        {
+            foreach (var item in items)
+                collection.Add(item);
+        }
+
+    }
+}
