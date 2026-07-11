5ca2474d98fb40f473a94cab16b9436198865a68
Chris Boot <chris@bootcom.co.uk>
2026-07-11 07:34:42 +0100

    Update nugets Add timings for the sync processing service

--- Common/MongoDB.Sync.MAUI/Services/InitialSyncProcessingService.cs
+++ Common/MongoDB.Sync.MAUI/Services/InitialSyncProcessingService.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Messaging;
 using MongoDB.Sync.Messages;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 
 namespace MongoDB.Sync.MAUI.Services
@@ -16,12 +17,26 @@ namespace MongoDB.Sync.MAUI.Services
         [ObservableProperty]
         string processingMessage = string.Empty;
 
+        [ObservableProperty]
+        TimeSpan lastSyncTaken = TimeSpan.Zero;
+
         private readonly IMessenger _messenger;
 
+        private Stopwatch _stopWatch;
+
         public InitialSyncProcessingService(IMessenger messenger) { 
+            
             _messenger = messenger;
 
             _messenger.Register<APISyncStartedMessage>(this, (r, m) => {
+                if (_stopWatch is null)
+                {
+                    _stopWatch = Stopwatch.StartNew();
+                }
+                else
+                {
+                    _stopWatch.Restart();
+                }
                 ProcessingMessage = "Sync Started";
                 IsRunning = true;
             });
@@ -32,6 +47,8 @@ namespace MongoDB.Sync.MAUI.Services
 
             _messenger.Register<APISyncCompletedMessage>(this, (r, m) =>
             {
+                _stopWatch?.Stop();
+                LastSyncTaken = _stopWatch?.Elapsed ?? TimeSpan.Zero;
                 ProcessingMessage = "Sync Complete";
                 IsRunning = false;
             });
